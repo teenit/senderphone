006b0c137b11a0871a9978f73f6c03c7efe9922d
Pavlo <teenitclub@gmail.com>
2024-01-02 17:28:09 +0200

    Start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='color'> Test</div>
+     
+    </div>
+  );
+}
+
+export default App;
